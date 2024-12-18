b35f7527ad3dcdbfc306f0feb42e58119cbb4465
SueiShen <XDAndy930712@gmail.com>
2024-12-18 18:33:54 +0800

    專案建立
    Android Studio版本:Ladybug 2024.2.1 Patch 3
    Git版本:2.47.1
    okhttp:5.0.0-alpha.14
    lifecycle-viewmodel-ktx:2.8.7
    lifecycle-livedata-ktx:2.8.7
    activity-ktx:1.9.3
    viewbinding設定

---
+++ app/src/main/java/com/example/android_ex/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.android_ex
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_ex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
