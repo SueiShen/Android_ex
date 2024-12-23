c3329bde43edfcaa5510423b4228e49c03b96aaf
SueiShen <XDAndy930712@gmail.com>
2024-12-23 23:06:38 +0800

    傳感器
    Sensor設定完成

--- app/src/main/java/com/example/android_ex/MainActivity.kt
+++ app/src/main/java/com/example/android_ex/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.android_ex
 
+import android.hardware.SensorManager
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +14,10 @@ import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.android_ex.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),SensorEventListener {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+    private lateinit var mSensor:Sensor
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -20,6 +27,11 @@ class MainActivity : AppCompatActivity() {
         /*supportFragmentManager.commit {
             replace(R.id.fragmentContainerView,DefaultFragment())
         }*/
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) as Sensor
+
+
+
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainerView, DefaultFragment())
             .commit()
@@ -40,5 +52,31 @@ class MainActivity : AppCompatActivity() {
                 replace(R.id.fragmentContainerView, TrafficFragment())
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        when(event?.sensor?.type){
+            Sensor.TYPE_ACCELEROMETER ->{
+                var x = event.values[0]
+                if (x>0){
+                    supportFragmentManager.commit {
+                        replace(R.id.fragmentContainerView, TrafficFragment())
+                    }
+                }else if (x<0) {
+                    supportFragmentManager.commit {
+                        replace(R.id.fragmentContainerView, WeatherFragment())
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
     }
 }
